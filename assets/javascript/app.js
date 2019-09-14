607aab43295c4c6685d716eff52825a38df70781
Jerry Dudum <jsdudum@gmail.com>
2019-09-13 19:59:35 -0700

    added basic structure of firebase to javascript and changed firebase reference in html

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDKd9dUWojUS8agnkFTkUT48BJt9J8q2MM",
+    authDomain: "train-scheduler-b2f2c.firebaseapp.com",
+    databaseURL: "https://train-scheduler-b2f2c.firebaseio.com",
+    projectId: "train-scheduler-b2f2c",
+    storageBucket: "",
+    messagingSenderId: "384480561690",
+    appId: "1:384480561690:web:caf699376c280269a89fa3"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+
